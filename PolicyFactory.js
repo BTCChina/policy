38d49cdce6f550fc0ccc2182c4a041ca8f9d4190
Frank Yao <frank@neon.local>
2016-12-07 15:46:57 +0800

    fix

--- PolicyFactory.js
+++ PolicyFactory.js
@@ -1,4 +1,4 @@
-const ErrorThrowerFactory = require('cyanide-error').ErrorThrowerFactory
+const ErrorThrowerFactory = require('phoenix-error').ErrorThrowerFactory
 
 /** Builds Policies */
 class PolicyFactory {
